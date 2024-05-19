6652d944bf339325fe501924f0706955b9a5054d
Soubghos <soubhik.ghosh@nokia.com>
2024-05-20 02:39:26 +0530

    models

---
+++ backend/models/chatModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const chatSchema = mongoose.Schema(
+    {
+        chatName: {
+            required: true,
+            type: String,
+            trim: true
+        },
+
+        isGroupChat: {
+            required: true,
+            type: Boolean,
+            default: false
+        },
+
+        users: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }],
+
+        latestMessage: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Message"
+        }],
+
+        groupAdmin: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+
+    }, { timestamps: true }
+)
+
+const Chat = mongoose.model("Chat", chatSchema);
+moduel.exports = Chat;
---
+++ backend/models/messageModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const messageSchema = mongoose.Schema(
+    {
+        sender: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+
+        chat: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Chat"
+        },
+
+        content: [{
+            type: String,
+            trime: true
+        }],
+
+    }, { timestamps: true }
+)
+
+const Message = mongoose.model("Message", messageSchema);
+moduel.exports = Message;
