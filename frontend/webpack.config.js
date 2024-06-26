b70a89bcf1f3cc777e35ead01bce4f78a4cec295
Soubhik <soubhik285@gmail.com>
2024-06-26 23:03:43 +0530

    webpack file for frontend

---
+++ frontend/webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.mjs$/,
+          include: /node_modules/,
+          type: 'javascript/auto'
+        }
+      ]
+    }
+  };
+  
