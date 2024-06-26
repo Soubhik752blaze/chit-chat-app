9a8f1f0bc1d96f9da924c08c629d2b28afeff683
Soubhik <soubhik285@gmail.com>
2024-06-26 23:05:32 +0530

    frontend assets

---
+++ frontend/src/Assets/constants.js
@@ -0,0 +1,20 @@
+export const defaultUserpic = "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"
+
+export const picTypes = ["image/jpeg", "image/png"];
+
+export const modalBoxStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    borderRadius: '10px',
+    bgcolor: 'background.paper',
+    border: '1px solid #eee',
+    boxShadow: 24,
+    p: 4,
+    pb: 3
+};
